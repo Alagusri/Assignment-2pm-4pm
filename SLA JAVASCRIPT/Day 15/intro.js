c905d2ef73f1e579e79dcdcad63dbe5dcbf43261
Alagusri <srialagu881@gmail.com>
2025-12-05 22:25:18 +0530

    update

---
+++ SLA JAVASCRIPT/Day 15/intro.js
@@ -0,0 +1,67 @@
+//what does js do:
+
+//change html element
+//change css style
+//respond to button clicks
+//validate forms
+//create annimations
+//build full apps (ReadableByteStreamController, Node.js, etc)
+
+
+//where js runs:
+
+//browser, chrome , edge, firefox, safari suite , brave, opera, onion tor
+//node.js = runs js outside the browser
+
+//js engine
+
+//chrome - v8 engine
+//firefox - spidermonkey
+//safari - javascript core 
+
+//reads the js code line by line
+/*
+console.log();
+console.log(25);
+console.log(true);
+console.log({name : "", age:});
+*/
+
+//function ar()
+//{
+//    alert('hello')
+// }
+
+// let, var, const
+
+//var a = 10;
+
+//a = 40; // reassignment
+
+//var a = 30; // redeclaring
+
+//{
+//    var a = 70;
+//}
+
+//console.log(a);
+
+//let b = 40; 
+//b = 50; // reassigning is possible in let
+
+//b = 89;
+
+//{
+    //let x = 90;
+  //  console.log(x);
+//}
+
+//let x = 77;
+//console.log(x);
+
+//let b = 98; //redeclaring is not possible 
+
+//console.log(b);
+const ar = 90
+
+console.log(ar);
