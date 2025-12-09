fd78ecb3ca6cc9c48b4db333e4f0628475f11b33
Alagusri <srialagu881@gmail.com>
2025-12-09 14:28:59 +0530

    update

---
+++ SLA JAVASCRIPT/Day 18/script.js
@@ -0,0 +1,25 @@
+//Conditions:
+
+//if (condition) {
+// code block
+// }
+
+// let age = 21;
+// if ( age >= 18) {
+//    console.log("you are an adult.");
+// }
+// else {
+//    console.log("you are a minor.");
+// }
+
+//checks only one condition
+//runs only when condition is true
+
+// if else statement
+// runs one block if true, another if false
+
+if(marks >= 35){
+    console.log("pass");
+} else{
+    console.log("fail");
+}
