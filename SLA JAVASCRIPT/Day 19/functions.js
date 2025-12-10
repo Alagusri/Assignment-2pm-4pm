412a1590db54dcb39a336c1518db39f430f9837f
Alagusri <srialagu881@gmail.com>
2025-12-10 14:36:46 +0530

    Create functions.js

---
+++ SLA JAVASCRIPT/Day 19/functions.js
@@ -0,0 +1,24 @@
+//function:
+
+//function can run only ween called
+//can be defined anywhere in script
+//good for reusability
+
+//when used:
+
+//button
+//calculations
+//validations
+//repeat a task
+
+//function expression:
+
+//function stored in a variable is called as function expression
+
+function great(){
+    console.log("hello world");
+}
+
+const show = function(){
+    console.log("function expression");
+}
