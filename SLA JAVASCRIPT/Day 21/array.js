0f1fc6ba8d78f049dd7ebf5964b92da323c69f7f
Alagusri <srialagu881@gmail.com>
2025-12-12 15:28:21 +0530

    update

---
+++ SLA JAVASCRIPT/Day 21/array.js
@@ -0,0 +1,7 @@
+let fruits =["apple", "banana","mango", "mesabi", "pine"]
+
+fruits.pop()
+fruits.shift()
+fruits.unshift("grapes")
+
+console.log(fruits);
---
+++ SLA JAVASCRIPT/Day 21/arrays.js
@@ -0,0 +1,7 @@
+let nums = [1,2,3]
+
+//nums.forEach(n => console.log(n))
+
+let doubled = nums.map(n => n*2);
+
+console.log(doubled);
