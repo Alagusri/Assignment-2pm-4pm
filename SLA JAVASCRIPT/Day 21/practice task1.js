76ae40d578af084a4e19122be785a069390bbcd4
Alagusri <srialagu881@gmail.com>
2025-12-19 14:09:01 +0530

    created

---
+++ SLA JAVASCRIPT/Day 21/practice task1.js
@@ -0,0 +1,6 @@
+const colors = ['Red' , 'Green' , 'Blue' , 'Orange' , 'violet']
+{
+
+    console.log(colors [0]);
+    console.log(colors[colors.length - 1]);
+}
---
+++ SLA JAVASCRIPT/Day 21/practice task2.js
@@ -0,0 +1,11 @@
+const items = ["Pen" , "Pencil"];
+{
+
+items.push("Scale");
+items.pop();
+
+items.unshift("Ball");
+items.shift();
+}
+
+console.log(items);
