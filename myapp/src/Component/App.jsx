b115b43896025471bea6130d733e1958e67b0d40
Alagusri <srialagu881@gmail.com>
2026-01-12 14:23:24 +0530

    updated

---
+++ myapp/src/Component/App.jsx
@@ -0,0 +1,25 @@
+import './app.css'
+import Header from './Component/Header'
+import UserCard from './Component/UserCard'
+import Welcome from './Component/Welcome'
+
+function App() {
+    const handleClick =() => {
+        alert("Button clicked")
+    }
+
+    return (
+        <>
+        <h1 className="card">Hello ar</h1>
+        <button onClick={handleClick}>click me</button>
+
+        <Welcome/>
+        <Header/>
+
+        <Button brs={handleClick}>click me</Button>
+
+        <UserCard name="Arun" age={25} isOnline ={true} skills={["React", "JS", "Angular"]}/>
+
+        </>
+    )
+}
---
+++ myapp/src/Component/UserCard.jsx
@@ -0,0 +1,14 @@
+function UserCard({Name, Age, skills, onclick}){
+    return (
+        <>
+        <h2>Name:(name)</h2>
+        <p>Age:(page)</p>
+        <ol style={}>
+            {skills.map(skill =>(<li key={skill}>{skill}</li>
+            ) )}
+        </ol>
+        </>
+    )
+}
+
+export default UserCard
