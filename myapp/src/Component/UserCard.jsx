b1e3d0b9709ae390f8859e0975203424d927acfc
Alagusri <srialagu881@gmail.com>
2026-01-14 09:10:05 +0530

    Update UserCard.jsx

--- myapp/src/Component/UserCard.jsx
+++ myapp/src/Component/UserCard.jsx
@@ -3,7 +3,7 @@ function UserCard({Name, Age, skills, onclick}){
         <>
         <h2>Name:(name)</h2>
         <p>Age:(page)</p>
-        <ol style={}>
+        <ol style={UserCard}>
             {skills.map(skill =>(<li key={skill}>{skill}</li>
             ) )}
         </ol>
